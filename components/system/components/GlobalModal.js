c00a8a10c5d7fec4bd07646107fa603fd7a45599
Martina <martinalong10@gmail.com>
2020-07-14 16:14:32 -0700

    additional style edits

--- components/system/components/GlobalModal.js
+++ components/system/components/GlobalModal.js
@@ -7,8 +7,8 @@ import { css } from "@emotion/react";
 
 const STYLES_BACKGROUND = css`
   position: fixed;
-  left: 0;
-  top: 0;
+  right: 0;
+  bottom: 0;
   width: 100%;
   height: 100%;
   display: flex;

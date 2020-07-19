ee0f241ce2b3a351e01fb5282a49152c706d3f14
tarafanlin <35607644+tarafanlin@users.noreply.github.com>
2020-07-18 17:58:25 -0700

    design system: new text styles and display css values

--- components/system/index.js
+++ components/system/index.js
@@ -53,7 +53,7 @@ import { TabGroup } from "~/components/system/components/TabGroup";
 import { Table } from "~/components/system/components/Table";
 import { Textarea } from "~/components/system/components/Textarea";
 import { Toggle } from "~/components/system/components/Toggle";
-import { H1, H2, H3, H4, P, UL, LI } from "~/components/system/components/Typography";
+import { H1, H2, H3, H4, P, UL, OL, LI } from "~/components/system/components/Typography";
 
 // NOTE(jim): Fragments
 import { CodeText } from "~/components/system/components/fragments/CodeText";
@@ -113,6 +113,7 @@ export {
   H4,
   P,
   UL,
+  OL,
   LI,
   // NOTE(jim): Fragments, not meant to be used.
   TooltipAnchor,

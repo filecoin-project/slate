61be06634c6eb7b003b07a4b418f602aa2b7d747
jimmylee <send@jimmy.email>
2020-07-29 16:14:26 -0700

    node constants: updates grpcweb route https://grpcweb.slate.textile.io

--- node_common/constants.js
+++ node_common/constants.js
@@ -1,4 +1,4 @@
 export const POLLING_RATE = 5000;
-export const POWERGATE_HOST = "http://pow.slate.textile.io:6002";
+export const POWERGATE_HOST = "https://grpcweb.slate.textile.io";
 export const FILE_STORAGE_URL = "./public/static/files/";
-export const GITHUB_URL = "https://github.com/filecoin-project/filecoin-client";
+export const GITHUB_URL = "https://github.com/filecoin-project/slate";

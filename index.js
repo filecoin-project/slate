053a8bf11c39e78dad15a809468cec7f0fca6e44
uonai <cs.mccaleb@gmail.com>
2020-08-01 13:25:24 -0500

    Electron env updates

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-require('@babel/register')({
+const path = require("path");
+const dirPath = path.join(__dirname);
+require("@babel/register")({
   presets: [
-    [require.resolve('@babel/preset-env')],
+    [require.resolve("@babel/preset-env")],
     [
-      require.resolve('next/babel'),
+      require.resolve("next/babel"),
       {
-        'preset-env': {},
-        'transform-runtime': {},
-        'styled-jsx': {},
-        'class-properties': {},
+        "preset-env": {},
+        "transform-runtime": {},
+        "styled-jsx": {},
+        "class-properties": {},
       },
     ],
   ],
-  plugins: [[require.resolve('@babel/plugin-transform-runtime')]],
-  ignore: ['node_modules', '.next'],
+  plugins: [
+    [require.resolve("@babel/plugin-transform-runtime")],
+    [
+      require.resolve("babel-plugin-module-resolver"),
+      {
+        alias: {
+          "~": dirPath,
+        },
+      },
+    ],
+  ],
+  ignore: ["node_modules", ".next"],
 });
 
-module.exports = require('./server.js');
+module.exports = require("./server.js");

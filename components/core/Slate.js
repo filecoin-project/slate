83872f3fe9cac04f17c002fdf67e620e207cfc00
jimmylee <send@jimmy.email>
2020-08-08 15:19:35 -0700

    slates: more mobile friendly

--- components/core/Slate.js
+++ components/core/Slate.js
@@ -1,88 +1,20 @@
 import * as React from "react";
 import * as Constants from "~/common/constants";
-import * as System from "~/components/system";
 
 import { css } from "@emotion/react";
+
+import SlateMediaObjectPreview from "~/components/core/SlateMediaObjectPreview";
 
 const STYLES_SLATE = css`
   display: flex;
   align-items: flex-start;
   flex-wrap: wrap;
   justify-content: flex-start;
-`;
 
-const STYLES_ITEM = css`
-  width: 288px;
-  height: 288px;
-  padding: 24px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  transition: 200ms ease all;
-`;
-
-const STYLES_IMAGE = css`
-  display: block;
-  max-width: 100%;
-  max-height: 100%;
-  cursor: pointer;
-`;
-
-const STYLES_PDF = css`
-  height: 100%;
-  width: 100%;
-  border: 1px solid ${Constants.system.border};
-  font-size: 24px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-`;
-
-class Item extends React.Component {
-  render() {
-    if (this.props.type && this.props.type.startsWith("video/")) {
-      return (
-        <span css={STYLES_ITEM}>
-          <div css={STYLES_PDF} onClick={this.props.onClick}>
-            Video
-          </div>
-        </span>
-      );
-    }
-
-    if (this.props.type && this.props.type.startsWith("audio/")) {
-      return (
-        <span css={STYLES_ITEM}>
-          <div css={STYLES_PDF} onClick={this.props.onClick}>
-            Audio
-          </div>
-        </span>
-      );
-    }
-
-    if (this.props.type && this.props.type.startsWith("application/pdf")) {
-      return (
-        <span css={STYLES_ITEM}>
-          <div css={STYLES_PDF} onClick={this.props.onClick}>
-            PDF
-          </div>
-        </span>
-      );
-    }
-
-    return (
-      <span css={STYLES_ITEM}>
-        <img
-          css={STYLES_IMAGE}
-          src={this.props.url}
-          onClick={this.props.onClick}
-        />
-      </span>
-    );
+  @media (max-width: ${Constants.sizes.mobile}px) {
+    justify-content: center;
   }
-}
+`;
 
 export default class Slate extends React.Component {
   render() {
@@ -90,7 +22,7 @@ export default class Slate extends React.Component {
       <div css={STYLES_SLATE}>
         {this.props.items.map((each, index) => {
           return (
-            <Item
+            <SlateMediaObjectPreview
               key={each.id}
               type={each.type}
               onClick={() => this.props.onSelect(index)}
---
+++ components/core/SlateMediaObjectPreview.js
@@ -0,0 +1,73 @@
+import * as React from "react";
+import * as Constants from "~/common/constants";
+
+import { css } from "@emotion/react";
+
+const STYLES_ITEM = css`
+  width: 288px;
+  height: 288px;
+  padding: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  transition: 200ms ease all;
+`;
+
+const STYLES_IMAGE = css`
+  display: block;
+  max-width: 100%;
+  max-height: 100%;
+  cursor: pointer;
+`;
+
+const STYLES_PDF = css`
+  height: 100%;
+  width: 100%;
+  border: 1px solid ${Constants.system.border};
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+export default class SlateMediaObjectPreview extends React.Component {
+  render() {
+    if (this.props.type && this.props.type.startsWith("video/")) {
+      return (
+        <span css={STYLES_ITEM}>
+          <div css={STYLES_PDF} onClick={this.props.onClick}>
+            Video
+          </div>
+        </span>
+      );
+    }
+
+    if (this.props.type && this.props.type.startsWith("audio/")) {
+      return (
+        <span css={STYLES_ITEM}>
+          <div css={STYLES_PDF} onClick={this.props.onClick}>
+            Audio
+          </div>
+        </span>
+      );
+    }
+
+    if (this.props.type && this.props.type.startsWith("application/pdf")) {
+      return (
+        <span css={STYLES_ITEM}>
+          <div css={STYLES_PDF} onClick={this.props.onClick}>
+            PDF
+          </div>
+        </span>
+      );
+    }
+
+    return (
+      <span css={STYLES_ITEM}>
+        <img css={STYLES_IMAGE} src={this.props.url} onClick={this.props.onClick} />
+      </span>
+    );
+  }
+}

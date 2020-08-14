28fd1fdb9303db637eee1fa27bb5fc8a71b10a84
Jordan Booker <jordan.m.booker@gmail.com>
2020-08-14 03:09:44 -0400

    Added content to new-index page.

--- pages/new-index.js
+++ pages/new-index.js
@@ -3,7 +3,7 @@ import * as Constants from "~/common/constants";
 import * as Actions from "~/common/actions";
 import * as System from "~/components/system";
 
-import { css } from "@emotion/react";
+import { css, keyframes } from "@emotion/react";
 
 import WebsitePrototypeWrapper from "~/components/core/WebsitePrototypeWrapper";
 import WebsitePrototypeHeader from "~/components/core/WebsitePrototypeHeader";
@@ -116,6 +116,75 @@ img:hover {
 }
 `;
 
+const STYLES_SIMPLE_FLOW_CONTAINER = css `
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  justify-content: space-around;
+  padding-top: 20vh;
+  align-items: center;
+`;
+
+const STYLES_ARROW_CONTAINER = css `
+  width: 200px;
+`
+
+const STYLES_SECTION_TEXT = css`
+  display: block;
+  max-width: 980px;
+`;
+
+const STYLES_SECTION_SVG_CONTAINER = css`
+  display: flex;
+  justify-content: center;
+`;
+
+const STYLES_STROKE_KF = keyframes`
+ from { stroke-dashoffset: 1; }
+ to { stroke-dashoffset: 0;}
+`
+
+const STYLES_SVG_AN = css`
+  animation: ${STYLES_STROKE_KF} 5s ease-in-out infinite alternate;
+`
+
+const STYLES_CONTR_CONTAINER = css `
+  display: flex;
+`
+const STYLES_CONTR_LIST = css `
+  display: flex;
+  justify-content: space-around;
+  width: 100vw;
+  list-style: none;
+`
+
+const STYLES_CONTR_LI0 = css `
+  font-size: 1.953rem;
+  color: ${Constants.system.black};
+  text-decoration: none;
+  padding-bottom: 1.5rem;
+`
+const STYLES_CONTR_LI1 = css `
+  font-size: 1.953rem;
+  color: ${Constants.system.black};
+  text-decoration: none;
+  opacity: .76;
+  padding-bottom: 1.5rem;
+`
+const STYLES_CONTR_LI2 = css `
+  font-size: 1.953rem;
+  color: ${Constants.system.black};
+  text-decoration: none;
+  opacity: .56;
+  padding-bottom: 1.5rem;
+`
+const STYLES_CONTR_LI3 = css `
+  font-size: 1.953rem;
+  color: ${Constants.system.black};
+  text-decoration: none;
+  opacity: .26;
+  padding-bottom: 1.5rem;
+`
 
 export const getServerSideProps = async (context) => {
   return {
@@ -147,7 +216,36 @@ export default class IndexPage extends React.Component {
             </section>
             <section css={STYLES_SECTION_WHITE}>
               <h1>Simple, intuitive</h1>
-              <h2>Words about things</h2>
+              <h2>Break way from abstract visualizations of your files.<br/>Securely organize your data in a relatable way</h2>
+              <div css={STYLES_SIMPLE_FLOW_CONTAINER}>
+                <div>{/*<ButtonPrimary>Upload File</ButtonPrimary> Figure out hoiw to pull in correctly*/}<button>Upload File</button></div>
+                <div css={STYLES_ARROW_CONTAINER}>
+                  <svg viewBox="0 0 350 100">
+                    <defs>
+                      <marker id="arrowhead" markerWidth="10" markerHeight="7"
+                      refX="0" refY="1.5" orient="auto" fill="#1b1f23">
+                        <polygon points="0 0, 5 1.5, 0 3" />
+                      </marker>
+                    </defs>
+                    <line x1="5" y1="50" x2="250" y2="50" stroke="#1b1f23"
+                    stroke-width="8" marker-end="url(#arrowhead)" />
+                  </svg>
+                </div>
+                <div><p>Slate securely stores your file</p></div>
+                <div css={STYLES_ARROW_CONTAINER}>
+                  <svg viewBox="0 0 350 100">
+                    <defs>
+                      <marker id="arrowhead" markerWidth="10" markerHeight="7"
+                      refX="0" refY="1.5" orient="auto" fill="#1b1f23">
+                        <polygon points="0 0, 5 1.5, 0 3" />
+                      </marker>
+                    </defs>
+                    <line x1="5" y1="50" x2="250" y2="50" stroke="#1b1f23"
+                    stroke-width="8" marker-end="url(#arrowhead)" />
+                  </svg>
+                </div>
+                <div><p>Share!</p></div>
+              </div>
             </section>
       
             <section css={STYLES_SECTION_FOREGROUND}>
@@ -189,7 +287,22 @@ export default class IndexPage extends React.Component {
 
             <section css={STYLES_SECTION_FOREGROUND}>
               <h1>Open Source</h1>
-              <h2>Words about things</h2>
+              <h2 css={STYLES_SECTION_TEXT}>"Collaboration makes better software". <br/><br/>Pair that with Filecoin's transparent marketplace as Slates's foundation. And you got yourself real nice power couple right there.</h2>
+              <div css={STYLES_SECTION_SVG_CONTAINER}>
+                <svg viewBox="-15 125 750 750" width="800" height="800">
+                  <path css={STYLES_SVG_AN} stroke="#b2b2b2" fill="none" strokeWidth="3" strokeDasharray="1 1" pathLength="1"
+                    d="m 398.40437,595.27818 c 40.32082,-14.97419 61.36968,-43.81349 61.36968,-93.44762 0,-49.63413 -41.82527,-93.37059 -94.0866,-93.45552 -55.14896,-0.0896 -96.75182,43.64 -96.0866,93.45552 0.66523,49.81551 24.42164,82.95509 62.44642,94.93898 L 264.68924,762.47972 C 174.536,739.14196 82.84508,632.78774 82.84508,501.83056 c 0,-155.10914 124.63284,-280.84993 280.84238,-280.84993 156.20954,0 282.84239,125.74079 282.84239,280.84993 0,132.99462 -90.82075,237.8649 -182.89748,261.11527 z"
+                  />
+                </svg>
+              </div>
+              <div css={STYLES_CONTR_CONTAINER}>
+                <ul css={STYLES_CONTR_LIST}>
+                  <li css={STYLES_CONTR_LI0}>Jimmylee<br/>github.com/jimmylee</li>
+                  <li css={STYLES_CONTR_LI1}>Martinalong<br/>github.com/martinalong</li>
+                  <li css={STYLES_CONTR_LI2}>William Felker<br/>github.com/gndclouds</li>
+                  <li css={STYLES_CONTR_LI3}>Uonai<br/>github.com/uonai</li>
+                </ul>
+              </div>
             </section>
           <WebsitePrototypeFooter />
         </div>

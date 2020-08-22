cf52263332f6886696c799ecc5bee4b96ee2ab39
@wwwjim <send@jimmy.email>
2020-08-21 19:19:22 -0700

    lol who needs a system when you got grit

--- common/constants.js
+++ common/constants.js
@@ -9,20 +9,6 @@ export const sizes = {
   sidebar: 416,
   header: 72,
 };
-export const system = {
-  white: "#ffffff",
-  foreground: "#f7f7f7",
-  gray: "#e0e0e0",
-  border: "#d8d8d8",
-  darkGray: "#b2b2b2",
-  black: "#1b1f23",
-  pitchBlack: "#0c0c0c",
-  brand: "#0047FF",
-  link: "#2935ff",
-  green: "#28a745",
-  yellow: " #FFC940",
-  red: "#ff0000",
-};
 
 export const code = {};
 

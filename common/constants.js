378ec4604d49d6b775b22cdd5b8157d46a95749d
@wwwjim <send@jimmy.email>
2020-08-21 19:32:34 -0700

    adds missing fields

--- common/constants.js
+++ common/constants.js
@@ -25,6 +25,33 @@ export const system = {
   red: "#ff0000",
 };
 
+export const zindex = {
+  navigation: 1,
+  sidebar: 2,
+  header: 3,
+  modal: 4,
+  tooltip: 5,
+};
+
+export const font = {
+  text: `'inter-regular', -apple-system, BlinkMacSystemFont, arial, sans-serif`,
+  semiBold: `'inter-semi-bold', -apple-system, BlinkMacSystemFont, arial, sans-serif`,
+  medium: `'inter-medium', -apple-system, BlinkMacSystemFont, arial, sans-serif`,
+  mono: `'mono', monaco, monospace`,
+  monoBold: `'mono-bold', monaco, monospace`,
+  monoCode: `'fira-code-regular', mono, monospace`,
+  monoCodeBold: `'fira-code-bold', mono-bold, monospace`,
+  code: `'jet-brains-regular', mono, monospace`,
+  codeBold: `'jet-brains-bold', mono, monospace`,
+};
+
+export const typescale = {
+  lvl1: `1rem`,
+  lvl2: `1.25rem`,
+  lvl3: `1.563rem`,
+  lvl4: `1.953rem`,
+};
+
 export const code = {};
 
 export const theme = {

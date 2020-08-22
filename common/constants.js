dbf2f8c4f1e9006af3868eda9c081c8e90b39905
@wwwjim <send@jimmy.email>
2020-08-21 19:18:36 -0700

    just deleting things because

---
+++ common/constants.js
@@ -0,0 +1,34 @@
+export const values = {
+  version: "0.1.0",
+  sds: "0.1.0",
+};
+
+export const sizes = {
+  mobile: 768,
+  navigation: 288,
+  sidebar: 416,
+  header: 72,
+};
+export const system = {
+  white: "#ffffff",
+  foreground: "#f7f7f7",
+  gray: "#e0e0e0",
+  border: "#d8d8d8",
+  darkGray: "#b2b2b2",
+  black: "#1b1f23",
+  pitchBlack: "#0c0c0c",
+  brand: "#0047FF",
+  link: "#2935ff",
+  green: "#28a745",
+  yellow: " #FFC940",
+  red: "#ff0000",
+};
+
+export const code = {};
+
+export const theme = {
+  foreground: system.white,
+  ctaBackground: system.brand,
+  pageBackground: system.foreground,
+  pageText: system.black,
+};

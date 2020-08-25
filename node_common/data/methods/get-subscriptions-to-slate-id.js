1e628725fac0a2b6a4c0fcf72b4e2ab2ba820527
@wwwjim <send@jimmy.email>
2020-08-25 13:42:14 -0700

    slates: get-subscriptions-to-slate-id and get-subscriptions-to-user-id

---
+++ node_common/data/methods/get-subscriptions-to-slate-id.js
@@ -0,0 +1,24 @@
+import { runQuery } from "~/node_common/data/utilities";
+
+export default async ({ slateId }) => {
+  return await runQuery({
+    label: "GET_SUBSCRIPTIONS_TO_SLATE_ID",
+    queryFn: async (DB) => {
+      const query = await DB.select("*")
+        .from("subscriptions")
+        .where({ target_slate_id: slateId });
+
+      if (!query || query.error) {
+        return [];
+      }
+
+      return JSON.parse(JSON.stringify(query));
+    },
+    errorFn: async (e) => {
+      return {
+        error: "GET_SUBSCRIPTIONS_TO_SLATE_ID",
+        source: e,
+      };
+    },
+  });
+};

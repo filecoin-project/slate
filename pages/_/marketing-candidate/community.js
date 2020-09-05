935a3bd3d5a985e57cebf9fb9eafc5f5aff27da6
William Felker <gndclouds@icloud.com>
2020-09-05 23:49:52 +0800

    Refactored CSS and Layout

--- pages/_/marketing-candidate/community.js
+++ pages/_/marketing-candidate/community.js
@@ -85,9 +85,9 @@ const STYLES_A = css`
 `;
 
 const STYLES_IMG = css`
-  width: 100%;
-  :hover {
-  }
+  width: 150px;
+  border-radius: 4px;
+
   @media (max-width: ${Constants.sizes.mobile}px) {
   }
 `;
@@ -139,23 +139,44 @@ const STYLES_SECTION_WRAPPER = css`
   align-items: center;
   height: 55vh;
   margin: 10px auto;
-  :nth-child(even) {
-    margin: 150px auto;
-    flex-direction: row-reverse;
-  }
+  width: 100%;
+
   @media (max-width: ${Constants.sizes.mobile}px) {
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-    :nth-child(even) {
-      flex-direction: column;
-    }
   }
 `;
 
-const STYLES_SECTION_CHILD = css`
+const STYLES_SECTION_CHILD_FULL = css`
+  padding: 50px;
+  margin: 15px;
+  @media (max-width: ${Constants.sizes.mobile}px) {
+    width: 100%;
+    padding: 20px;
+  }
+`;
+const STYLES_SECTION_CHILD_SPLIT = css`
+  padding: 50px;
+  margin: 15px;
   width: 50%;
-  padding: 0 50px;
+  @media (max-width: ${Constants.sizes.mobile}px) {
+    width: 100%;
+    padding: 20px;
+  }
+`;
+const STYLES_CARD_WRAPPER = css`
+  float: left;
+  display: inline;
+  width: 150px;
+  transition: 200ms ease box-shadow;
+  border-radius: 4px;
+  box-shadow: 0px 4px 8px 4px rgba(0, 0, 0, 0.02);
+  margin: 20px;
+  :hover {
+    transition: 200ms ease box-shadow;
+    box-shadow: 0px 10px 40px 20px rgba(0, 0, 0, 0.1);
+  }
   @media (max-width: ${Constants.sizes.mobile}px) {
     width: 100%;
     padding: 20px;
@@ -189,49 +210,273 @@ export default class IndexPage extends React.Component {
         <WebsitePrototypeHeader />
         <div css={STYLES_ROOT}>
           <div css={STYLES_SECTION_WRAPPER}>
-            <div css={STYLES_SECTION_CHILD}>
+            <div css={STYLES_SECTION_CHILD_FULL}>
               <h1 css={STYLES_H1}>
                 Slate is designed and built by a growing community of hackers,
                 artists, and creatives on the web.
               </h1>
-              <p css={STYLES_H2}>
-                Take any image on the web and save it to Slate right from your
-                browser tab.
-              </p>
             </div>
-            <div css={STYLES_SECTION_CHILD}>
+            <div css={STYLES_SECTION_CHILD_FULL}>
               <img
                 css={STYLES_IMG}
-                src="https://d2w9rnfcy7mm78.cloudfront.net/8547413/original_613b9b0a650a3f274c68e1407f552ff3.png?1599111034?bc=0"
+                src=" https://bafybeihi2f53tmtwxv5f5jd2wcxtzrutgvxg5xsawfv3ousfjz2yfurm2i.ipfs.slate.textile.io"
                 alt="Slate browser extension"
               />
             </div>
           </div>
           <div css={STYLES_SECTION_WRAPPER}>
-            <div css={STYLES_SECTION_CHILD}>
-              <h1 css={STYLES_H1}>
-                Slate is designed and built by a growing community of hackers,
-                artists, and creatives on the web.
-              </h1>
-              <p css={STYLES_H2}>
-                Take any image on the web and save it to Slate right from your
-                browser tab.
-              </p>
-              <a src="/">
-                <button css={STYLES_BUTTON_PRIMARY}>
-                  Get Chrome Extension
-                </button>
-              </a>
-              <p css={STYLES_ANNOTATION}>
-                Currently avaible for <a href="/">Chrome</a>.
+            <div css={STYLES_SECTION_CHILD_FULL}>
+              <h1 css={STYLES_H1}>Core Team</h1>
+              <br />
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/jasonleyser">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Jason Leyser"
+                      src="https://bafkreie3dvujhpil4tgv2qx2lng5bfeif7reyybmgaftub6n4wxx4vnbly.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Jason Leyser</p>
+                  <p css={STYLES_P}>@jasonleyser</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/jimmylee">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Cake"
+                      src="https://bafkreibfbxpiypzw4t7ubvjsxac7z3fdigvran5sayp2ibbuxhvzeivn4a.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Cake</p>
+                  <p css={STYLES_P}>@jimmylee</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/martinalong">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Martina Long"
+                      src="https://bafkreif4mdwcxtykrcl2sfs6kcapg3d2tjyjjpcpmxnw3rqshbvr5ncjdi.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Martina Long</p>
+                  <p css={STYLES_P}>@martinalong</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/harisbutt">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Haris Butt"
+                      src="https://bafkreigs2w7wsambbek5v5e4ykkoketj6r6ho6t3snm7jso7hodyzxbqfu.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Haris Butt</p>
+                  <p css={STYLES_P}>@harisbutt</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/tarafanlin">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Tara Lin"
+                      src="https://bafkreibku3qhls572oo4hskrg4t32c2kcj4auujyxioz5i44pfbaatwqbe.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Tara Lin</p>
+                  <p css={STYLES_P}>@tarafanlin</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/gndclouds">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for William Felker"
+                      src="https://bafybeic25subftulrkrxrw2ggpjblamofj3uemi2vaoqmlqzyzg2lfji5q.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>William Felker</p>
+                  <p css={STYLES_P}>@gndclouds</p>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div css={STYLES_SECTION_WRAPPER}>
+            <div css={STYLES_SECTION_CHILD_FULL}>
+              <h1 css={STYLES_H1}>Contributors</h1>
+              <br />
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/pooja">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Pooja Shah"
+                      src="https://bafkreifqrmwuvlky7urkmkxyswksyjjpxvk62jwqgol35bfdfshgmcjmba.ipfs.slate.textile.io/"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Pooja Shah</p>
+                  <p css={STYLES_P}>@pooja</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/whyrusleeping">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Why"
+                      src="https://bafkreiczwqnp5c6msa42pihhobagcbq6r5lkxuucmm3rmccb5lh46x3h7u.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Pooja Shah</p>
+                  <p css={STYLES_P}>@pooja</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/asutula">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Aaron Sutula"
+                      src="https://bafkreihl4pll4esqivugvam7d7j6oxbms4kz6c3azq77vf2ittwuon2dy4.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Aaron Sutula</p>
+                  <p css={STYLES_P}>@asutula</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/jsign">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Ignacio Hagopian"
+                      src="https://bafkreieqpfn4bpqv3yrdr22surdngc3xyn574miybm3awjosfu6fcmbd6a.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Ignacio Hagopian</p>
+                  <p css={STYLES_P}>@jsign</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/sanderpick">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Sander Pick"
+                      src="https://bafkreial7bum4chyd2rubvxkyufis4qczvb7xerrzis7eg6gyshbf7ltci.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Sander Pick</p>
+                  <p css={STYLES_P}>@sanderpick</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/andrewxhill">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Andrew Hill"
+                      src="https://bafkreighz4m7bqjmt7cidgbbocbzp65f4liuzebwq6d64t27slhpe2cigm.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Andrew Hill</p>
+                  <p css={STYLES_P}>@andrewxhill</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/akuokojnr">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Akuoko Daniel Jnr"
+                      src="https://bafkreibpqkmnm6vijyxlkbcjyayoo7b6tnf4tzelzsi4wk3z6o7enhpbrm.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Akuoko Daniel Jnr</p>
+                  <p css={STYLES_P}>@akuokojnr</p>
+                </a>
+              </div>
+
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/narative">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Narative"
+                      src="https://bafkreihgmyxi2rzp4gtkoxwkrajiwivrbttixfjrdfb6qz5fhpdaofrhoi.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Narative</p>
+                  <p css={STYLES_P}>@narative</p>
+                </a>
+              </div>
+              <div css={STYLES_CARD_WRAPPER}>
+                <a href="https://github.com/narative">
+                  <div>
+                    <img
+                      css={STYLES_IMG}
+                      alt="Github Profile Photo for Colin S. Mccaleb"
+                      src="https://bafkreigxhplpm7adi3p77eljj3g66lcnnzig7m6ihpervxwqeti3tbqudi.ipfs.slate.textile.io"
+                    />
+                  </div>
+                  <p css={STYLES_P}>Colin S. McCaleb</p>
+                  <p css={STYLES_P}>@uonai</p>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div css={STYLES_SECTION_WRAPPER}>
+            <div css={STYLES_SECTION_CHILD_FULL}>
+              <h1 css={STYLES_H1}>Get involved</h1>
+              <p css={STYLES_P}>
+                Slate is a fully open-source file sharing network designed for
+                research and collaboration.
               </p>
             </div>
-            <div css={STYLES_SECTION_CHILD}>
-              <img
-                css={STYLES_IMG}
-                src="https://d2w9rnfcy7mm78.cloudfront.net/8547413/original_613b9b0a650a3f274c68e1407f552ff3.png?1599111034?bc=0"
-                alt="Slate browser extension"
-              />
+            <div css={STYLES_SECTION_CHILD_SPLIT}>
+              <h2 css={STYLES_H2}>Contribute</h2>
+              <br />
+              <p css={STYLES_P}>
+                Get involved with the project and contribute.
+              </p>
+              <button>
+                <a>Github</a>
+              </button>
+            </div>
+            <div css={STYLES_SECTION_CHILD_SPLIT}>
+              <h2 css={STYLES_H2}>Contact</h2>
+              <br />
+              <p css={STYLES_P}>
+                {" "}
+                Reach out to any of the core contributors, reach us on Twitter,
+                or join our Slack.
+              </p>
+              <button>
+                <a>Github</a>
+              </button>
+            </div>
+            <div css={STYLES_SECTION_CHILD_SPLIT}>
+              <h2 css={STYLES_H2}>Integrate</h2>
+              <br />
+              <p css={STYLES_P}>
+                Explore our API and SDK and build on top of Slate.
+              </p>
+              <CodeBlock>npm install --save slate-react-system</CodeBlock>
+            </div>
+            <div css={STYLES_SECTION_CHILD_SPLIT}>
+              <h2 css={STYLES_H2}>Design System</h2>
+              <br />
+              <p css={STYLES_P}>
+                Check out our open source design system for your projects.
+              </p>
+              <CodeBlock>npm install --save slate-react-system</CodeBlock>
             </div>
           </div>
         </div>

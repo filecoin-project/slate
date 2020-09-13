1cbe0381938cb2b8835d4354635260cf98deb39e
Martina <martinalong10@gmail.com>
2020-09-12 21:47:20 -0700

    fixed user/slate deletion bug

--- node_common/data/methods/delete-slate-by-id.js
+++ node_common/data/methods/delete-slate-by-id.js
@@ -4,6 +4,10 @@ export default async ({ id }) => {
   return await runQuery({
     label: "DELETE_SLATE_BY_ID",
     queryFn: async (DB) => {
+      const subscriptions = await DB.from("subscriptions")
+        .where({ target_slate_id: id })
+        .del();
+
       const data = await DB.from("slates").where({ id }).del();
 
       return 1 === data;

bc8ec2db1f525af9105dea0f15e3e86fda49cc30
@wwwjim <send@jimmy.email>
2020-09-21 18:09:28 -0700

    dead code elimination on SceneHome

--- scenes/SceneHome.js
+++ scenes/SceneHome.js
@@ -9,11 +9,6 @@ import ScenePage from "~/components/core/ScenePage";
 import DataView from "~/components/core/DataView";
 import ScenePageHeader from "~/components/core/ScenePageHeader";
 
-const STYLES_NUMBER = css`
-  font-family: ${Constants.font.semiBold};
-  font-weight: 400;
-`;
-
 const STYLES_VIDEO_BIG = css`
   display: block;
   background-color: ${Constants.system.moonstone};
@@ -43,77 +38,6 @@ export default class SceneHome extends React.Component {
   };
 
   render() {
-    // TODO(jim): Refactor later.
-    const slates = {
-      columns: [
-        {
-          key: "name",
-          name: "Slate Name",
-          width: "100%",
-          type: "SLATE_LINK",
-        },
-        { key: "url", width: "268px", name: "URL", type: "NEW_WINDOW" },
-        { key: "id", id: "id", name: "Slate ID", width: "296px" },
-        {
-          key: "objects",
-          name: "Objects",
-        },
-        {
-          key: "public",
-          name: "Public",
-          type: "SLATE_PUBLIC_TEXT_TAG",
-          width: "188px",
-        },
-      ],
-      rows: this.props.viewer.slates.map((each) => {
-        return {
-          ...each,
-          url: `https://slate.host/${this.props.viewer.username}/${each.slatename}`,
-          name: each.data.name,
-          public: each.data.public,
-          objects: <span css={STYLES_NUMBER}>{each.data.objects.length}</span>,
-        };
-      }),
-    };
-
-    // TODO(jim): Refactor later.
-    const slateButtons = [
-      { name: "Create slate", type: "SIDEBAR", value: "SIDEBAR_CREATE_SLATE" },
-    ];
-
-    /*
-    // TODO(jim): Refactor later.
-    const wallet = {
-      columns: [
-        { key: "address", name: "Address" },
-        { key: "balance", name: "Filecoin", width: "228px" },
-        { key: "type", name: "Type", width: "188px", type: "TEXT_TAG" },
-      ],
-      rows: this.props.viewer.addresses,
-    };
-
-    // TODO(jim): Refactor later.
-    const walletButtons = [
-      {
-        name: "View all",
-        type: "NAVIGATE",
-        value: "V1_NAVIGATION_WALLET",
-      },
-    ];
-    */
-
-    /*
-      {this.props.viewer.addresses[0] ? (
-    <Section title="Wallet addresses" buttons={walletButtons} onAction={this.props.onAction}>
-      <System.Table
-        data={wallet}
-        name="transaction"
-        onAction={this.props.onAction}
-        onNavigateTo={this.props.onNavigateTo}
-      />
-    </Section>
-  ) : null}
-  */
     let hasChildren = false;
     if (this.props.viewer && this.props.viewer.library[0].children.length) {
       hasChildren = true;

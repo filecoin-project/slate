1531e1628a04eeb45ed8210d4377cc417282ee42
tarafanlin <35607644+tarafanlin@users.noreply.github.com>
2020-10-21 20:34:38 -0700

    new navigation with CTA

--- components/core/WebsitePrototypeHeader.js
+++ components/core/WebsitePrototypeHeader.js
@@ -1,97 +1,81 @@
 import * as React from "react";
 import * as Constants from "~/common/constants";
-import * as SVGLogo from "~/common/logo";
 
+import { Logo } from "~/common/logo.js";
 import { css } from "@emotion/react";
 
-const STYLES_CONTAINER = css`
-  font-family: ${Constants.font.code};
-  text-transform: uppercase;
-  font-size: 12px;
+const STYLES_ROOT = css`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  padding: 24px 88px 24px 64px;
   width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 16px 24px 16px 24px;
+  height: 100%;
+  margin: 0 auto;
+  z-index: ${Constants.zindex.header};
 
   @media (max-width: ${Constants.sizes.mobile}px) {
-    display: block;
+    padding: 16px 24px;
+  }
+`;
+
+const STYLES_CONTAINER = css`
+  max-width: 1440px;
+  margin: 0 auto;
+  font-family: ${Constants.font.text};
+  font-weight: 400;
+  font-size: 1rem;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  @media (max-width: ${Constants.sizes.mobile}px) {
+    display: flex;
+    justify-content: space-between;
   }
 `;
 
 const STYLES_LINK = css`
-  color: ${Constants.system.pitchBlack};
+  color: ${Constants.system.grayBlack};
   text-decoration: none;
   transition: 200ms ease color;
   display: block;
 
-  :visited {
-    color: ${Constants.system.black};
-  }
-
   :hover {
     color: ${Constants.system.brand};
-  }
-
-  @media (max-width: ${Constants.sizes.mobile}px) {
-    padding: 16px 0 0 0;
   }
 `;
 
 const STYLES_LEFT = css`
   flex-shrink: 0;
-  padding: 0 8px 0 8px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  text-align: left;
-
-  @media (max-width: ${Constants.sizes.mobile}px) {
-    display: block;
-  }
+  height: 24px;
 `;
 
 const STYLES_RIGHT = css`
   min-width: 10%;
   width: 100%;
   display: flex;
-  align-items: center;
   justify-content: flex-end;
   text-align: left;
-  padding: 0 8px 0 8px;
-
-  @media (max-width: ${Constants.sizes.mobile}px) {
-    display: block;
-    text-align: left;
-  }
 `;
 
 const WebsitePrototypeHeader = (props) => {
   return (
-    <div css={STYLES_CONTAINER} style={props.style}>
-      <div css={STYLES_LEFT}>
-        <a
-          css={STYLES_LINK}
-          href="/"
-          style={{ marginRight: 16, position: "relative", top: "1px" }}
-        >
-          <SVGLogo.Symbol height={`20px`} />
-        </a>
-        <a
-          css={STYLES_LINK}
-          href="/"
-          style={{ marginRight: 24, fontFamily: Constants.font.codeBold }}
-        >
-          {Constants.values.version}
-        </a>
-        <a css={STYLES_LINK} href="/_/system">
-          Design System
-        </a>
-      </div>
-      <div css={STYLES_RIGHT}>
-        <a css={STYLES_LINK} href="https://github.com/filecoin-project/slate">
-          View Source
-        </a>
+    <div css={STYLES_ROOT}>
+      <div css={STYLES_CONTAINER} style={props.style}>
+        <div css={STYLES_LEFT}>
+          <a
+            css={STYLES_LINK}
+            href="/"
+            style={{ marginRight: 16, position: "relative", top: "1px" }}
+          >
+            <Logo style={{ height: 20 }} />
+          </a>
+        </div>
+        <div css={STYLES_RIGHT}>
+          <a css={STYLES_LINK} href="/_">
+            Sign up⼁Log in
+          </a>
+        </div>
       </div>
     </div>
   );

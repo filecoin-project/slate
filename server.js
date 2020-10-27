08680b78a2cf9efa1320cf238eab2b2477d8fe04
jimmylee <send@jimmy.email>
2020-10-27 11:07:26 -0700

    removes unused sleep method

--- server.js
+++ server.js
@@ -22,12 +22,6 @@ const app = next({
   quiet: false,
 });
 
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
 const handler = app.getRequestHandler();
 
 const EXTERNAL_RESOURCES = {

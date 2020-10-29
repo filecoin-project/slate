020f4f4f547ddbb908821fe217c0923e76ef88f7
tarafanlin <35607644+tarafanlin@users.noreply.github.com>
2020-10-29 10:43:04 -0700

    got help from Martina for the new feature announcement modal

--- components/core/OnboardingModal.js
+++ components/core/OnboardingModal.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 import * as Constants from "~/common/constants";
+import * as Actions from "~/common/actions";
 
 import { css } from "@emotion/react";
-import { ButtonPrimary } from "~/components/system/components/Buttons";
+import { ButtonPrimary, ButtonSeconddary } from "~/components/system/components/Buttons";
 import { dispatchCustomEvent } from "~/common/custom-events";
+import { CullFaceNone, NoBlending } from "three";
 
 const STYLES_MODAL = css`
   text-align: center;
@@ -24,6 +26,7 @@ const STYLES_BUTTON_SECONDARY = {
   color: Constants.system.brand,
   marginRight: 16,
   width: 160,
+  textDecoration: `none`,
 };
 
 const STYLES_IMAGE = css`
@@ -45,9 +48,76 @@ const STYLES_TEXT = css`
   padding: 0 64px;
 `;
 
+export const announcements = [
+  {
+    title: "New Feature",
+    text: "Organize files in grid layout",
+    image: (
+      <img
+        src="https://slate.textile.io/ipfs/bafybeiaiy3c72sdirjj24vvev7xpuvvqckwip3ot7l4u4iaxdlnbp4hqae"
+        alt="grid layout"
+        css={STYLES_IMAGE}
+      />
+    ),
+    button: (
+      <ButtonPrimary
+        style={{ width: 160 }}
+        onClick={() => {
+          dispatchCustomEvent({
+            name: "delete-modal",
+            detail: {},
+          });
+        }}
+      >
+        Got it
+      </ButtonPrimary>
+    ),
+  },
+  {
+    title: "New Feature 2",
+    text: "Organize files in grid layout",
+    image: (
+      <img
+        src="https://slate.textile.io/ipfs/bafybeiaiy3c72sdirjj24vvev7xpuvvqckwip3ot7l4u4iaxdlnbp4hqae"
+        alt="grid layout"
+        css={STYLES_IMAGE}
+      />
+    ),
+    button: (
+      <ButtonPrimary
+        style={{ width: 160 }}
+        onClick={() => {
+          dispatchCustomEvent({
+            name: "delete-modal",
+            detail: {},
+          });
+        }}
+      >
+        Got it
+      </ButtonPrimary>
+    ),
+  },
+];
+
 export class OnboardingModal extends React.Component {
   state = {
     step: 0,
+    slides: [],
+  };
+
+  componentDidMount = () => {
+    const newAccount = this.props.newAccount;
+    Actions.updateOnboardingStatus({ onboarding: this.props.unseenAnnouncements });
+    let toShow = [];
+    if (newAccount) {
+      toShow = this.onboardingCopy;
+    }
+    for (let feature of announcements) {
+      if (this.props.unseenAnnouncements.includes(feature.title)) {
+        toShow.push(feature);
+      }
+    }
+    this.setState({ slides: toShow });
   };
 
   onboardingCopy = [
@@ -81,11 +151,13 @@ export class OnboardingModal extends React.Component {
       ),
       button: (
         <React.Fragment>
-          {/* <a href={null} target="_blank">
-            <ButtonPrimary style={STYLES_BUTTON_SECONDARY}>
-              Get extension
-            </ButtonPrimary>
-          </a> */}
+          <a
+            href="https://chrome.google.com/webstore/detail/slate/gloembacbehhbfbkcfjmloikeeaebnoc"
+            target="_blank"
+            style={{ textDecoration: `none` }}
+          >
+            <ButtonPrimary style={STYLES_BUTTON_SECONDARY}>Get extension</ButtonPrimary>
+          </a>
           <ButtonPrimary style={{ width: 160 }} onClick={() => this._handleClick(1)}>
             Next
           </ButtonPrimary>
@@ -104,15 +176,7 @@ export class OnboardingModal extends React.Component {
         />
       ),
       button: (
-        <ButtonPrimary
-          style={{ width: 160 }}
-          onClick={() => {
-            dispatchCustomEvent({
-              name: "delete-modal",
-              detail: {},
-            });
-          }}
-        >
+        <ButtonPrimary style={{ width: 160 }} onClick={() => this._handleClick(1)}>
           Start using Slate
         </ButtonPrimary>
       ),
@@ -120,16 +184,31 @@ export class OnboardingModal extends React.Component {
   ];
 
   _handleClick = (i) => {
-    this.setState({ step: this.state.step + i });
+    if (this.state.step + i >= this.state.slides.length) {
+      dispatchCustomEvent({
+        name: "delete-modal",
+        detail: {},
+      });
+    } else {
+      this.setState({ step: this.state.step + i });
+    }
   };
 
   render() {
+    if (!this.state.slides || !this.state.slides.length) {
+      return null;
+    }
+
     return (
-      <div css={STYLES_MODAL}>
-        {this.onboardingCopy[this.state.step].image}
-        <div css={STYLES_TITLE}>{this.onboardingCopy[this.state.step].title}</div>
-        <div css={STYLES_TEXT}>{this.onboardingCopy[this.state.step].text}</div>
-        {this.onboardingCopy[this.state.step].button}
+      <div>
+        <div css={STYLES_MODAL}>
+          <div>
+            {this.state.slides[this.state.step].image}
+            <div css={STYLES_TITLE}>{this.state.slides[this.state.step].title}</div>
+            <div css={STYLES_TEXT}>{this.state.slides[this.state.step].text}</div>
+            {this.state.slides[this.state.step].button}
+          </div>
+        </div>
       </div>
     );
   }

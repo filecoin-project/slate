e7e8520eb08536e1218836bdbc35c2e6d9292991
jimmylee <send@jimmy.email>
2020-11-16 23:50:08 -0800

    adds remainder of droptable fields

--- scripts/drop-database.js
+++ scripts/drop-database.js
@@ -12,8 +12,14 @@ console.log(`RUNNING:  drop-database.js`);
 Promise.all([
   db.schema.dropTable("users"),
   db.schema.dropTable("slates"),
+  db.schema.dropTable("pending"),
+  db.schema.dropTable("activity"),
+  db.schema.dropTable("trusted"),
+  db.schema.dropTable("subscriptions"),
+  db.schema.dropTable("keys"),
   db.schema.dropTable("keys"),
   db.schema.dropTable("stats"),
+  db.schema.dropTable('deals'),
   db.schema.dropTable("orphans")
 ]);
 

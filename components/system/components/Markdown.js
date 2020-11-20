89e26cfc1d07a3bb1939b5acb5a8d32ea54f374b
Chris Waring <chris@wwaves.co>
2020-11-20 18:16:07 +0000

    enable remark-gfm

--- components/system/components/Markdown.js
+++ components/system/components/Markdown.js
@@ -3,6 +3,7 @@ import * as React from "react";
 import unified from "unified";
 import parse from "remark-parse";
 import remark2react from "remark-react";
+import gfm from "remark-gfm";
 import linkifyRegex from "remark-linkify-regex";
 
 export const Markdown = ({ md, options }) => {
@@ -11,6 +12,7 @@ export const Markdown = ({ md, options }) => {
       {
         unified()
           .use(parse)
+          .use(gfm)
           .use(linkifyRegex(/@(\w*[0-9a-zA-Z-_]+\w*[0-9a-zA-Z-_])/g)) // @user
           .use(linkifyRegex(/^(https?):\/\/[^\s$.?#].[^\s]*$/gm)) // http(s) links
           .use(remark2react, options)

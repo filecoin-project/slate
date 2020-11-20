3b0e95bdf62f3727304b893c59bfd3b2e54aedb9
Chris Waring <chris@wwaves.co>
2020-11-20 15:17:55 +0000

    add regex for deeplinks and mentions

--- components/system/components/Markdown.js
+++ components/system/components/Markdown.js
@@ -1,11 +1,20 @@
 import * as React from "react";
 
-import unified from 'unified'
-import parse from 'remark-parse'
-import remark2react from 'remark-react'
+import unified from "unified";
+import parse from "remark-parse";
+import remark2react from "remark-react";
+import linkifyRegex from "remark-linkify-regex";
 
-export const Markdown = ({md, options}) => {
-  return <React.Fragment>
-    {unified().use(parse).use(remark2react, options).processSync(md).result}
-  </React.Fragment>
-}
+export const Markdown = ({ md, options }) => {
+  return (
+    <React.Fragment>
+      {
+        unified()
+          .use(parse)
+          .use(linkifyRegex(/[@#](\w*[0-9a-zA-Z-_]+\w*[0-9a-zA-Z-_])/g))
+          .use(remark2react, options)
+          .processSync(md).result
+      }
+    </React.Fragment>
+  );
+};

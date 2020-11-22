89f34a86fcb714cfc25275317c7e9f727714aeec
@wwwjim <send@jimmy.email>
2020-11-21 23:31:31 -0800

    this function didn't even make sense

--- common/state.js
+++ common/state.js
@@ -38,25 +38,6 @@ const transformPeers = (peersList = []) => {
   });
 };
 
-export const getSelectedState = (props) => {
-  if (!props) {
-    return null;
-  }
-
-  const { info } = props;
-
-  if (!info || !info.id) {
-    return {
-      address: "",
-    };
-  }
-
-  return {
-    address: null,
-    address: info.defaultStorageConfig.cold.filecoin.addr,
-  };
-};
-
 export const getInitialState = (props) => {
   if (!props) {
     return null;
@@ -99,24 +80,18 @@ export const _deprecated_getInitialState = (props) => {
     // NOTE(jim): Powergate Hot Settings
     settings_hot_enabled: info.defaultStorageConfig.hot.enabled,
     settings_hot_allow_unfreeze: info.defaultStorageConfig.hot.allowUnfreeze,
-    settings_hot_ipfs_add_timeout:
-      info.defaultStorageConfig.hot.ipfs.addTimeout,
+    settings_hot_ipfs_add_timeout: info.defaultStorageConfig.hot.ipfs.addTimeout,
 
     // NOTE(jim): Powergate Cold Settings
     settings_cold_enabled: info.defaultStorageConfig.cold.enabled,
     settings_cold_default_address: info.defaultStorageConfig.cold.filecoin.addr,
-    settings_cold_default_duration:
-      info.defaultStorageConfig.cold.filecoin.dealMinDuration,
-    settings_cold_default_replication_factor:
-      info.defaultStorageConfig.cold.filecoin.repFactor,
+    settings_cold_default_duration: info.defaultStorageConfig.cold.filecoin.dealMinDuration,
+    settings_cold_default_replication_factor: info.defaultStorageConfig.cold.filecoin.repFactor,
     settings_cold_default_excluded_miners:
       info.defaultStorageConfig.cold.filecoin.excludedMinersList,
-    settings_cold_default_trusted_miners:
-      info.defaultStorageConfig.cold.filecoin.trustedMinersList,
-    settings_cold_default_max_price:
-      info.defaultStorageConfig.cold.filecoin.maxPrice,
-    settings_cold_default_auto_renew:
-      info.defaultStorageConfig.cold.filecoin.renew.enabled,
+    settings_cold_default_trusted_miners: info.defaultStorageConfig.cold.filecoin.trustedMinersList,
+    settings_cold_default_max_price: info.defaultStorageConfig.cold.filecoin.maxPrice,
+    settings_cold_default_auto_renew: info.defaultStorageConfig.cold.filecoin.renew.enabled,
     settings_cold_default_auto_renew_max_price:
       info.defaultStorageConfig.cold.filecoin.renew.threshold,
 

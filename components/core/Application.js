6d29342792fa13f44c858a6b7b53960c98c9657e
Martina <martinalong10@gmail.com>
2021-01-09 13:26:30 -0800

    removed unnecessary log

--- components/core/Application.js
+++ components/core/Application.js
@@ -613,8 +613,8 @@ export default class ApplicationPage extends React.Component {
     const description = "";
     const url = "https://slate.host/_";
 
-    console.log("application state:", { target: current.target });
-    console.log("application state:", { data: this.state.data });
+    // console.log("application state:", { target: current.target });
+    // console.log("application state:", { data: this.state.data });
 
     return (
       <React.Fragment>

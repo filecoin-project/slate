af087024359cc28a8b015442d640b7da8f58664c
Martina <martinalong10@gmail.com>
2021-01-17 12:57:34 -0800

    added endpoint to navigate by slate or user id

--- server.js
+++ server.js
@@ -148,6 +148,50 @@ app.prepare().then(async () => {
   server.all("/_/:a", async (r, s) => handler(r, s, r.url));
   server.all("/_/:a/:b", async (r, s) => handler(r, s, r.url));
 
+  server.get("/[$]/slate/:id", async (req, res) => {
+    const slate = await Data.getSlateById({
+      id: req.params.id,
+    });
+
+    if (!slate) {
+      return res.redirect("/404");
+    }
+
+    if (slate.error) {
+      return res.redirect("/404");
+    }
+
+    if (!slate.data.public) {
+      return res.redirect("/403");
+    }
+
+    const creator = await Data.getUserById({ id: slate.data.ownerId });
+
+    if (!creator) {
+      return res.redirect("/404");
+    }
+
+    if (creator.error) {
+      return res.redirect("/404");
+    }
+
+    return res.redirect(`/${creator.username}/${slate.slatename}`);
+  });
+
+  server.get("/[$]/user/:id", async (req, res) => {
+    const creator = await Data.getUserById({ id: req.params.id });
+
+    if (!creator) {
+      return res.redirect("/404");
+    }
+
+    if (creator.error) {
+      return res.redirect("/404");
+    }
+
+    return res.redirect(`/${creator.username}`);
+  });
+
   server.get("/[$]/:id", async (req, res) => {
     let mobile = Window.isMobileBrowser(req.headers["user-agent"]);
 
@@ -286,50 +330,50 @@ app.prepare().then(async () => {
 
     creator.subscribers = r2.serializedSubscribers;
 
-    let exploreSlates = [];
+    // let exploreSlates = [];
 
-    if (Environment.IS_PRODUCTION) {
-      exploreSlates = await Data.getSlatesByIds({
-        ids: [
-          //NOTE(tara): slates in prod
-          "d2861ac4-fc41-4c07-8f21-d0bf06be364c",
-          "9c2c458c-d92a-4e81-a4b6-bf6ab4607470",
-          "7f461144-0647-43d7-8294-788b37ae5979",
-          "f72c2594-b8ac-41f6-91e0-b2da6788ae23",
-          "a0d6e2f2-564d-47ed-bf56-13c42634703d",
-          "0ba92c73-92e7-4b00-900e-afae4856c9ea",
-        ],
-      });
+    // if (Environment.IS_PRODUCTION) {
+    //   exploreSlates = await Data.getSlatesByIds({
+    //     ids: [
+    //       //NOTE(tara): slates in prod
+    //       "d2861ac4-fc41-4c07-8f21-d0bf06be364c",
+    //       "9c2c458c-d92a-4e81-a4b6-bf6ab4607470",
+    //       "7f461144-0647-43d7-8294-788b37ae5979",
+    //       "f72c2594-b8ac-41f6-91e0-b2da6788ae23",
+    //       "a0d6e2f2-564d-47ed-bf56-13c42634703d",
+    //       "0ba92c73-92e7-4b00-900e-afae4856c9ea",
+    //     ],
+    //   });
 
-      for (let exploreSlate of exploreSlates) {
-        let user = await Data.getUserById({ id: exploreSlate.data.ownerId });
-        exploreSlate.username = user.username;
-      }
-    } else {
-      exploreSlates = await Data.getSlatesByIds({
-        ids: [
-          //NOTE(tara): slates in localhost for testing
-          "857ad84d-7eff-4861-a988-65c84b62fc23",
-          "81fa0b39-0e96-4c7f-8587-38468bb67cb3",
-          "c4e8dad7-4ba0-4f25-a92a-c73ef5522d29",
-          "df05cb1f-2ecf-4872-b111-c4b8493d08f8",
-          "435035e6-dee4-4bbf-9521-64c219a527e7",
-          "ac907aa3-2fb2-46fd-8eba-ec8ceb87b5eb",
-        ],
-      });
+    //   for (let exploreSlate of exploreSlates) {
+    //     let user = await Data.getUserById({ id: exploreSlate.data.ownerId });
+    //     exploreSlate.username = user.username;
+    //   }
+    // } else {
+    //   exploreSlates = await Data.getSlatesByIds({
+    //     ids: [
+    //       //NOTE(tara): slates in localhost for testing
+    //       "857ad84d-7eff-4861-a988-65c84b62fc23",
+    //       "81fa0b39-0e96-4c7f-8587-38468bb67cb3",
+    //       "c4e8dad7-4ba0-4f25-a92a-c73ef5522d29",
+    //       "df05cb1f-2ecf-4872-b111-c4b8493d08f8",
+    //       "435035e6-dee4-4bbf-9521-64c219a527e7",
+    //       "ac907aa3-2fb2-46fd-8eba-ec8ceb87b5eb",
+    //     ],
+    //   });
 
-      for (let exploreSlate of exploreSlates) {
-        let user = await Data.getUserById({ id: exploreSlate.data.ownerId });
-        exploreSlate.username = user.username;
-      }
-    }
+    //   for (let exploreSlate of exploreSlates) {
+    //     let user = await Data.getUserById({ id: exploreSlate.data.ownerId });
+    //     exploreSlate.username = user.username;
+    //   }
+    // }
 
     return app.render(req, res, "/_/profile", {
       viewer,
       creator,
       mobile,
       resources: EXTERNAL_RESOURCES,
-      exploreSlates,
+      // exploreSlates,
     });
   });
 

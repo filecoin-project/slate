44cd0a80ce50f0aa6ee61e3f938047f1ded4df4f
toast <toastts@icloud.com>
2021-02-19 11:00:47 -0700

    privacy filter bug fix

--- scenes/SceneFilesFolder.js
+++ scenes/SceneFilesFolder.js
@@ -23,9 +23,9 @@ const POLLING_INTERVAL = 10000;
 const STYLES_FILTERS_CONTAINER = css`
   height: 60px;
   display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: flex-start;
+  flex-direction: row-reverse;
+  justify-content: flex-start;
+  align-items: center;
 `;
 
 //TODO(toast): Constants for SDS in future
@@ -85,7 +85,6 @@ export default class SceneFilesFolder extends React.Component {
 
   _handlePrivacyFilter = (filter) => {
     const viewer = this.props.viewer;
-    const filtered = getPublicAndPrivateFiles({ viewer });
     if (filter === "All") {
       this.setState({ privacy: "All" }, this._filterFiles);
     } else if (filter === "Public") {
@@ -93,7 +92,7 @@ export default class SceneFilesFolder extends React.Component {
     } else if (filter === "Private") {
       this.setState({ privacy: "Private" }, this._filterFiles);
     } else {
-      console.log("this is the worst possible scenario");
+      console.log("unusable privacy param");
     }
   };
 
@@ -104,11 +103,11 @@ export default class SceneFilesFolder extends React.Component {
     if (filter === "All") {
       return viewer.library[0].children;
     } else if (filter === "Public") {
-      return filtered.PublicFiles;
+      return filtered.publicFiles;
     } else if (filter === "Private") {
-      return filtered.PrivateFiles;
+      return filtered.privateFiles;
     } else {
-      console.log("this is the worst possible scenario");
+      console.log("unusable privacy param");
     }
   };
 
@@ -126,18 +125,14 @@ export default class SceneFilesFolder extends React.Component {
 
   _filterFiles = () => {
     const filteredFiles = [];
-    const library = this._getPrivacyFiles(this.state.privacy);
+    const privacy = this.state.privacy;
+    const library = this._getPrivacyFiles(privacy);
     const filters = this.state.filters;
     const filterKeys = Object.keys(filters).filter((key) => {
       return filters[key] === true;
     });
 
-    for (const type in filterKeys) {
-      for (const mimeType in Constants.filetypes[type]) {
-      }
-    }
-
-    if (filterKeys.length && library.length) {
+    if (filterKeys.length && library?.length) {
       for (const libraryObject of library) {
         for (const type of filterKeys) {
           for (const mimeType of Constants.filetypes[type]) {
@@ -260,6 +255,52 @@ export default class SceneFilesFolder extends React.Component {
         />
         <div css={STYLES_FILTERS_CONTAINER}>
           <div style={{ position: "relative" }}>
+            <ButtonTertiary onClick={this._handlePrivacyTooltip}>
+              <span style={{ width: 50 }}>{this.state.privacy}</span>
+            </ButtonTertiary>
+            {this.state.privacyTooltip ? (
+              <Boundary
+                captureResize={true}
+                captureScroll={false}
+                enabled
+                onOutsideRectEvent={() => this.setState({ privacyTooltip: false })}
+              >
+                <div css={STYLES_TOOLTIP_ANCHOR} style={{ width: 91, left: 2, top: 44 }}>
+                  <div css={STYLES_PRIVACY_TOOLTIP}>
+                    <div
+                      style={{
+                        color: this.state.privacy === "All" ? Constants.system.brand : null,
+                        cursor: "pointer",
+                      }}
+                      onClick={() => this._handlePrivacyFilter("All")}
+                    >
+                      All
+                    </div>
+                    <div
+                      style={{
+                        color:
+                          this.state.privacy === "Private" ? Constants.system.brand : "inherit",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => this._handlePrivacyFilter("Private")}
+                    >
+                      Private
+                    </div>
+                    <div
+                      style={{
+                        color: this.state.privacy === "Public" ? Constants.system.brand : "inherit",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => this._handlePrivacyFilter("Public")}
+                    >
+                      Public
+                    </div>
+                  </div>
+                </div>
+              </Boundary>
+            ) : null}
+          </div>
+          <div style={{ position: "relative" }}>
             <ButtonTertiary style={{ marginRight: 20 }} onClick={this._handleFiletypeTooltip}>
               <SVG.Filter
                 height="18px"
@@ -360,52 +401,6 @@ export default class SceneFilesFolder extends React.Component {
                         Books
                       </span>
                     </CheckBox>
-                  </div>
-                </div>
-              </Boundary>
-            ) : null}
-          </div>
-          <div style={{ position: "relative" }}>
-            <ButtonTertiary onClick={this._handlePrivacyTooltip}>
-              <span style={{ width: 50 }}>{this.state.privacy}</span>
-            </ButtonTertiary>
-            {this.state.privacyTooltip ? (
-              <Boundary
-                captureResize={true}
-                captureScroll={false}
-                enabled
-                onOutsideRectEvent={() => this.setState({ privacyTooltip: false })}
-              >
-                <div css={STYLES_TOOLTIP_ANCHOR} style={{ width: 91, left: 2, top: 44 }}>
-                  <div css={STYLES_PRIVACY_TOOLTIP}>
-                    <div
-                      style={{
-                        color: this.state.privacy === "All" ? Constants.system.brand : null,
-                        cursor: "pointer",
-                      }}
-                      onClick={() => this._handlePrivacyFilter("All")}
-                    >
-                      All
-                    </div>
-                    <div
-                      style={{
-                        color:
-                          this.state.privacy === "Private" ? Constants.system.brand : "inherit",
-                        cursor: "pointer",
-                      }}
-                      onClick={() => this._handlePrivacyFilter("Private")}
-                    >
-                      Private
-                    </div>
-                    <div
-                      style={{
-                        color: this.state.privacy === "Public" ? Constants.system.brand : "inherit",
-                        cursor: "pointer",
-                      }}
-                      onClick={() => this._handlePrivacyFilter("Public")}
-                    >
-                      Public
-                    </div>
                   </div>
                 </div>
               </Boundary>
